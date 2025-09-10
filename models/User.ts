f31d528e5f14f5e7e4771cf1051712757b4d1f29
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-10 13:17:20 +0530

    make changes in api

--- models/User.ts
+++ models/User.ts
@@ -23,5 +23,9 @@ userSchema.pre('save',async function (next) {
     if(this.isModified("password")){
         this.password = await bcrypt.hash(this.password,10 )
     }
-    next()
-})
+    next();
+});
+
+const User = models?.User || model<IUser>("User",userSchema)
+
+export default User;
