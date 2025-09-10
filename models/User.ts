b1309630acf0d6c88e8052337236e50ffc90dfce
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-10 13:10:16 +0530

    make changes in api

--- models/User.ts
+++ models/User.ts
@@ -19,3 +19,9 @@ const userSchema = new Schema<IUser>(
     }
 );
 
+userSchema.pre('save',async function (next) {
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password,10 )
+    }
+    next()
+})
