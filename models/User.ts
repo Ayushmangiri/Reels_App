1621465132349ed7705b36f9172dd550425fd9f6
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-10 13:02:53 +0530

    Making the reels app like instagram

---
+++ models/User.ts
@@ -0,0 +1,21 @@
+import mongoose,{Schema,model,models} from "mongoose";
+import bcrypt from "bcryptjs";
+ export interface IUser {
+    emails : string;
+    password : string;
+    _id?: mongoose.Types.ObjectId;
+        createdAt?: Date;
+        updatedAt?: Date;
+}
+const userSchema = new Schema<IUser>(
+    {
+        emails:{type:String , required: true,unique:true },
+        password:{type:String,required:true }
+
+
+    },
+    {
+        timestamps:true
+    }
+);
+
