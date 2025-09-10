167b3ec7e7230f691802abb57da59af928705140
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-10 13:29:27 +0530

    make changes in api

--- models/Video.ts
+++ models/Video.ts
@@ -4,3 +4,34 @@ export const VIDEO_DIMENSIONS = {
     width : 1080,
     height : 1920
 }as const;
+
+export interface IVideo{
+    _id?:mongoose.Types.ObjectId;
+    title : string;
+    description:string;
+    thambnailUrl: string;
+    controls?:boolean;
+    transformation?: {
+        height : number;
+        width : number;
+        quality?: number;
+    };
+}
+
+const videoSchema = new Schema<IVideo>({
+           title:{type:String , required:true} ,
+            description:{type:String , required:true} ,
+            thambnailUrl:{type:String , required:true} ,
+            controls:{type:String , required:true} ,
+            transformation:{
+                    height:{type:Number , default: VIDEO_DIMENSIONS.height},
+                    width:{type:Number , default: VIDEO_DIMENSIONS.width},
+                    quality:{type:Number , min:1,max:100},
+
+
+            } ,
+    },
+   {
+    timestamps:true
+  }
+)
