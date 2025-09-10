766c7302a2d61ce97a80f8f061b43fab5942f642
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-10 16:45:18 +0530

    make changes in api

---
+++ api/auth/register/route.ts
@@ -0,0 +1,14 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { model } from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request:NextRequest) {
+    try{
+
+    }
+    catch(error){
+        
+    }
+    
+}
