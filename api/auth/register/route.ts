9b37bb2d5e3b15fb48f2b4a39b3d913fb9d73314
Ayushmangiri <ayushmangiri@gmail.com>
2025-09-10 17:03:33 +0530

    make changes in api

--- api/auth/register/route.ts
+++ api/auth/register/route.ts
@@ -1,14 +1,48 @@
 import { connectToDatabase } from "@/lib/db";
 import User from "@/models/User";
+import { error } from "console";
+import { create } from "domain";
 import { model } from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request:NextRequest) {
     try{
+            const {email , password } = await request.json()
+            if (!email || !password){
+                return NextResponse.json(
+                    {
+                        error:"Email and password are required "
+                    },
+                    {status:403}
+                )
+            }
+
+             await connectToDatabase()
+             const existingUser = await User.findOne({email})
+            if(existingUser){
+                    return NextResponse.json(
+                         {
+                        error:"User Already Registerd"
+                    },
+                    {status:400} 
+                );  
+                        }
+                        await User.create({
+                            email,
+                            password
+                        })
+                        return NextResponse.json(
+                            {message:"You are successfully Signup  !!!Please login !!"}
+                        );
+            
 
     }
     catch(error){
-        
+        console.error("Registration error")
+            return NextResponse.json(
+                {error : "Failed to register User"},
+                {status:400}
+            )
     }
     
 }
